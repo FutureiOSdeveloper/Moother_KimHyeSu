9a23646d309316097e1bafa45ae27303f0c0edc5
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-07-24 23:25:23 +0900

    [feat] #1 메인뷰 scrollview + 하단 view indicator

--- AppleWeather/AppleWeather/Sources/Main/ViewController.swift
+++ AppleWeather/AppleWeather/Sources/Main/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var cityList : [String] = []
 
+    @IBOutlet weak var scrollview: UIScrollView!
+    @IBOutlet weak var pagecontrol: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        setDummy()
+        setScrollviewUI()
         // Do any additional setup after loading the view.
     }
+    
+    func setScrollviewUI(){
+        scrollview.delegate = self
 
+        for index in 0..<cityList.count {
+            
+            let containView = UIView()
+            let sb = UIStoryboard(name: "WeatherDetail", bundle: nil)
+            let vc = sb.instantiateViewController(withIdentifier: WeatherDetailVC.identifier)
+            
+            containView.frame = UIScreen.main.bounds
+            containView.frame.origin.x = UIScreen.main.bounds.width * CGFloat(index)
+            scrollview.addSubview(containView)
+            containView.addSubview(vc.view)
+    
+            
+        }
+        
+        scrollview.contentSize = CGSize(
+            width: UIScreen.main.bounds.width * CGFloat(cityList.count), height: UIScreen.main.bounds.height
+        )
+        
+        scrollview.alwaysBounceVertical = false
+        pagecontrol.numberOfPages = cityList.count
+        
+    }
+    
+    func setDummy(){
+        cityList.append(contentsOf: ["서울", "부산", "마포구", "용산구", "수원시"])
 
+    }
+
+    @IBAction func pageChanged(_ sender: Any) {
+        UIView.animate(withDuration: 0.3){
+            self.scrollview.contentOffset.x = UIScreen.main.bounds.width * CGFloat(self.pagecontrol.currentPage)
+        }
+    }
+    
+}
+
+extension ViewController : UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pagecontrol.currentPage = Int(floor(scrollview.contentOffset.x / UIScreen.main.bounds.width))
+    }
 }
 
